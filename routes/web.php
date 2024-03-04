97fbf88558a7d02749304139efc97baebd385687
blokfish <157576934+blokfish@users.noreply.github.com>
2024-03-04 01:03:15 +0100

    Added Delete button and edit button

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
-    ->middleware(['auth', 'verified']);
+->only(['index', 'store', 'edit', 'update', 'destroy'])
+->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
